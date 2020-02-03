01392e05c91006264c8ad62619080b3aa9b893b2
basuanish <55137730+basuanish@users.noreply.github.com>
2020-02-03 11:28:42 +0530

    Driver Microservice structure

---
+++ Driver/src/main/java/com/capgemini/GoCab/Driver/DriverApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.GoCab.Driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverApplication.class, args);
+	}
+
+}
