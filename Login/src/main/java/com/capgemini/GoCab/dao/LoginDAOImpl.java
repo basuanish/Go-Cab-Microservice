18dd07c7fb52ecb3bc8ea43a34031a4017dff0cc
basuanish <55137730+basuanish@users.noreply.github.com>
2020-02-03 11:04:40 +0530

    Login Microservice structure

---
+++ Login/src/main/java/com/capgemini/GoCab/dao/LoginDAOImpl.java
@@ -0,0 +1,5 @@
+package com.capgemini.GoCab.dao;
+
+public class LoginDAOImpl implements LoginDAO {
+
+}
---
+++ Login/src/main/java/com/capgemini/GoCab/service/LoginServiceImpl.java
@@ -0,0 +1,5 @@
+package com.capgemini.GoCab.service;
+
+public class LoginServiceImpl implements LoginService {
+
+}
