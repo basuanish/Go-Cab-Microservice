f3169b9e6d7aebde0b7267c2fea87c5eb637d3b0
mansii-agarwal <55138466+mansii-agarwal@users.noreply.github.com>
2020-02-03 17:51:06 +0530

    updates

--- Payment/src/main/java/com/capgemini/GoCab/entity/PaymentEntity.java
+++ Payment/src/main/java/com/capgemini/GoCab/entity/PaymentEntity.java
@@ -1,5 +1,35 @@
 package com.capgemini.GoCab.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "payment")
+
 public class PaymentEntity {
 
+	@Id
+	@Column(name = "payment_id")
+	private int paymentId;
+	
+	@Column(name = "fare")
+	private int fare;
+
+	public int getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public int getFare() {
+		return fare;
+	}
+
+	public void setFare(int fare) {
+		this.fare = fare;
+	}
+	
+	
 }
--- Payment/src/main/java/com/capgemini/GoCab/service/PaymentServiceImpl.java
+++ Payment/src/main/java/com/capgemini/GoCab/service/PaymentServiceImpl.java
@@ -1,5 +1,22 @@
 package com.capgemini.GoCab.service;
 
-public class PaymentServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.capgemini.GoCab.dao.PaymentDAO;
+import com.capgemini.GoCab.dto.Payment;
+import com.capgemini.GoCab.entity.PaymentEntity;
+
+public class PaymentServiceImpl implements PaymentService{
+
+	@Autowired
+	PaymentDAO paymentDAO;
+	
+
+	@Override
+	public String addPayment(Payment payment)
+	{
+		PaymentEntity pE= new PaymentEntity();
+		
+		
+	}
 }
