d1356e4e29c032938c3db1946ec74ae11dab587e
basuanish <55137730+basuanish@users.noreply.github.com>
2020-02-03 15:13:30 +0530

    Driver registration

--- Driver/src/main/java/com/capgemini/GoCab/service/DriverService.java
+++ Driver/src/main/java/com/capgemini/GoCab/service/DriverService.java
@@ -1,5 +1,12 @@
 package com.capgemini.GoCab.service;
 
+import org.springframework.stereotype.Service;
+
+import com.capgemini.GoCab.dto.Driver;
+
+@Service
 public interface DriverService {
+	
+	public String addDriver(Driver driver);
 
 }
--- Driver/src/main/java/com/capgemini/GoCab/service/DriverServiceImpl.java
+++ Driver/src/main/java/com/capgemini/GoCab/service/DriverServiceImpl.java
@@ -1,5 +1,27 @@
 package com.capgemini.GoCab.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.capgemini.GoCab.dao.DriverDAO;
+import com.capgemini.GoCab.dto.Driver;
+import com.capgemini.GoCab.entity.DriverEntity;
+
 public class DriverServiceImpl implements DriverService {
+	
+	@Autowired
+	DriverDAO driverDAO;
+
+	@Override
+	public String addDriver(Driver driver) {
+		DriverEntity dE = new DriverEntity();
+		dE.setName(driver.getName());
+		dE.setAge(driver.getAge());
+		dE.setCarNumber(driver.getCarNumber());
+		dE.setCarType(driver.getCarType());
+		dE.setPhoneNumber(driver.getPhoneNumber());
+		
+		driverDAO.save(dE);
+		return "Driver added";
+	}
 
 }
