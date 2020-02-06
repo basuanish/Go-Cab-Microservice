d0f496b7317e784c4b873c3c531358eec965b658
mansii-agarwal <55138466+mansii-agarwal@users.noreply.github.com>
2020-02-06 15:19:29 +0530

    booking microservices

--- booking/src/main/java/com/capgemini/GoCab/booking/BookingApplication.java
+++ booking/src/main/java/com/capgemini/GoCab/booking/BookingApplication.java
@@ -1,4 +1,4 @@
-package com.pecunia.GoCab.booking;
+package com.capgemini.GoCab.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
