25861e3dffe8c9bf7b556f5d91e2004e2059607e
risrai <55137949+risrai@users.noreply.github.com>
2020-02-07 12:59:59 +0530

    transit constants file added

--- Transit/Transit/src/main/java/com/capgemini/GoCab/controller/TransitController.java
+++ Transit/Transit/src/main/java/com/capgemini/GoCab/controller/TransitController.java
@@ -3,14 +3,16 @@ package com.capgemini.GoCab.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capgemini.GoCab.constants.TransitConstants;
 import com.capgemini.GoCab.model.TransitModel;
 import com.capgemini.GoCab.repository.TransitRepository;
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping(path="/transit")
 @RestController
 public class TransitController {
@@ -22,7 +24,8 @@ public class TransitController {
 	@GetMapping(path="/search")
 	List <TransitModel>findByQuery()
 	{
-		return transitRepository.findByCabTypeAndStatus("Sedan", "Active") ;
+
+		return transitRepository.findByCabTypeAndStatus(TransitConstants.CabType, TransitConstants.Status) ;
 	}
 			
 }
